6311f9dbd7e4b9652bc5711a0587b77ae047ea17
Akash Bulla <bulla1623@gmail.com>
2024-04-04 05:01:52 +0530

    Information Updated

--- JS/main.js
+++ JS/main.js
@@ -1,67 +1,78 @@
-// /*Toggle navbar*/
-// let menuIcon = document.querySelector('.menu-icon');
-// let navbar = document.querySelector('.navbar');
+const menuIcon = document.querySelector("#menu-icon");
+const navbar = document.querySelector(".navbar");
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("header nav a");
 
-// menuIcon.addEventListener('click', () => {
-//     navbar.classList.toggle('active');
-//     menuIcon.classList.toggle('fa-bars');
-//     menuIcon.classList.toggle('fa-xmark');
-// });
+menuIcon.addEventListener("click", () => {
+    navbar.classList.toggle("active");
+    menuIcon.classList.toggle("fa-bars");
+    menuIcon.classList.toggle("fa-xmark");
+});
 
-function toggleNavbar() {
-    const navbar = document.querySelector('.navbar');
-    const menuIcon = document.querySelector('.menu-icon');
-    navbar.classList.toggle('active');
-    menuIcon.classList.toggle('fa-bars');
-    menuIcon.classList.toggle('fa-xmark');
-}
-
-/* Scroll Active link */
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
-
-window.addEventListener('scroll', () => {
-    sections.forEach(sec => {
+window.addEventListener("scroll", () => {
+    sections.forEach((sec) => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
+        let id = sec.getAttribute("id");
 
         if (top >= offset && top < offset + height) {
-            navLinks.forEach(link => {
-                link.classList.remove('active');
+            navLinks.forEach((link) => {
+                link.classList.remove("active");
             });
-            document.querySelector('header nav a[href="#' + id + '"]').classList.add('active');
+            document
+                .querySelector('header nav a[href="#' + id + '"]')
+                .classList.add("active");
         }
     });
 
-    /**Sticky navbar */
-    let header = document.querySelector('header');
-    header.classList.toggle('sticky', window.scrollY > 100);
+    const header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 100);
 
-    /**remove toggle icon and navbar*/
-    menuIcon.classList.remove('fa-xmark');
-    navbar.classList.remove('active');
+    if (navbar.classList.contains("active")) {
+        menuIcon.classList.remove("fa-xmark");
+        navbar.classList.remove("active");
+    }
 });
 
-/**Scroll reveal */
+const scrollRevealConfig = (options) => {
+    ScrollReveal().reveal(options.elements, options.config);
+};
 
-/*
-ScrollReveal({
-    distance: '80px',
-    duration: 2000,
-    delay: 200,
+const homeContentConfig = {
+    elements: ".home-content, .heading",
+    config: { origin: "top", distance: "80px", duration: 2000, delay: 200 },
+};
+
+const homeImgConfig = {
+    elements: ".home-img, .services-container, .portfolio-box, .contact-form",
+    config: { origin: "bottom", distance: "80px", duration: 2000, delay: 200 },
+};
+
+const homeContactConfig = {
+    elements: ".home-contact h1, .about-img",
+    config: { origin: "left", distance: "80px", duration: 2000, delay: 200 },
+};
+
+const aboutContentConfig = {
+    elements: ".home-contact p, .about-content",
+    config: { origin: "right", distance: "80px", duration: 2000, delay: 200 },
+};
+
+scrollRevealConfig(homeContentConfig);
+scrollRevealConfig(homeImgConfig);
+scrollRevealConfig(homeContactConfig);
+scrollRevealConfig(aboutContentConfig);
+
+ScrollReveal().reveal(".contact-form", {
+    origin: "bottom",
+    distance: "100px",
+    duration: 2500,
+    delay: 500,
 });
 
-*/
-ScrollReveal().reveal('.home-content, .heading', { origin: 'top', distance: '80px', duration: 2000, delay: 200 });
-ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact-form', { origin: 'bottom', distance: '80px', duration: 2000, delay: 200 });
-ScrollReveal().reveal('.home-contact h1, .about-img', { origin: 'left', distance: '80px', duration: 2000, delay: 200 });
-ScrollReveal().reveal('.home-contact p, .about-content', { origin: 'right', distance: '80px', duration: 2000, delay: 200 });
-
-/***Typed JS */
-const typed = new Typed('.multiple-text', {
-    strings: ['Developer', 'Artist', 'Photolover'],
+const typed = new Typed(".multiple-text", {
+    strings: ["Developer", "Artist", "Photolover"],
     typeSpeed: 70,
     backSpeed: 70,
     backDelay: 1000,
